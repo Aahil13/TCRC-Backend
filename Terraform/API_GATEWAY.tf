e5072d9aa80e6607522f9b1fb97e5222fd49bf10
Aahil13 <onyeanunaprince@example.com>
2023-11-05 17:05:59 +0100

    Update API Gateway CORS configuration to allow access from S3 bucket and CloudFront distribution

--- Terraform/API_GATEWAY.tf
+++ Terraform/API_GATEWAY.tf
@@ -1,19 +1,9 @@
-module "s3_bucket" {
-  source = "./S3_bucket.tf"
-}
-
-module "cloudfront" {
-  source = "./Cloudfront.tf"
-}
-
-
-
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "resume_counter_api"
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = ["http://127.0.0.1:5500", module.s3_bucket.s3_bucket_url, module.cloudfront.cloudfront_url]
+    allow_origins = ["http://127.0.0.1:5500", "http://${aws_s3_bucket.resume_bucket.bucket}.s3-website.us-east-1.amazonaws.com", "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"]
     allow_methods = ["GET"]
     allow_headers = ["Content-Type"]
     max_age       = 300
